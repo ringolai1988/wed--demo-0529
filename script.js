168deb63f372894d2d517baa8659b5da14462613
Ringo <misaocute@gmail.com>
2022-05-29 15:10:32 +0800

    upup

---
+++ script.js
@@ -0,0 +1,13 @@
+let listBtn = document.getElementById("list-btn");
+let closeBtn = document.getElementById("close-btn");
+let mobileMenu = document.getElementById("menu-mobile");
+
+
+listBtn.onclick = () => {
+  mobileMenu.classList.toggle("active");
+}    
+
+
+closeBtn.onclick = () => {
+  mobileMenu.classList.toggle("active");
+}
